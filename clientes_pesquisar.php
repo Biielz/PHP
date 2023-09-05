43db7f3bdb882cf300ab640705c0a14b2577efc4
Bii <105325395+Biielz@users.noreply.github.com>
2023-09-05 08:47:16 -0300

    Add files via upload

--- clientes_pesquisar.php
+++ clientes_pesquisar.php
@@ -4,35 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Controle de Despesas - Pesquisa</title>
+    <link rel="stylesheet" href="estilos_menu.css">
+    <link rel="stylesheet" href="estilos_formulario.css">
 </head>
 <body>
     <?php
-        require "menu.php"
+        require "menu.php";
+
+        echo "<h3>Listagem dos Clientes</h3>";
+        require "conexao.php";
+
+        $sql = "SELECT * FROM clientes ORDER BY nome";
+        $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+        echo "<table border='1' width='1000' align='center'>";
+            echo "<tr>";
+                echo "<th width='100' align='right'>Código</th>";
+                echo "<th width='300' align='Left'>Nome</th>";
+                echo "<th width='100' align='Left'>CPF</th>";
+                echo "<th width='250' align='Left'>E-mail</th>";
+                echo "<th width='50' align='Left'>Editar</th>";
+            echo "</tr>";
+            while ($linha=mysqli_fetch_array($resultado)) {
+                $codigo = $linha["codigo"];
+                $nome = $linha["nome"];
+                $cpf = $linha["cpf"];
+                $email = $linha["email"];
+
+                echo "<tr>";
+                    echo "<th width='100' align='right'>$codigo</th>";
+                    echo "<th width='300' align='Left'>$nome</th>";
+                    echo "<th width='100' align='Left'>$cpf</th>";
+                    echo "<th width='250' align='Left'>$email</th>";
+                    echo "<th width='50' align='center'><a href='clientes_editar.php?codigo=$codigo'>Editar</a></th>";
+                echo "</tr>";
+            }
+        echo "</table>";
     ?>
-    echo "<h3>Listagem dos Clientes</h3>";
-    require "conexao.php";
-    $sql="SELECT * FROM clientes ORDER BY nome";
-    $resultado=mysql_query($conexao, $sql) or die(mysqli_error($conexao));
-    echo "<table border='1' width='1000' align='center'>";
-        echo "<tr>";
-            echo "<th width='100' align='right'>Código</th>";
-            echo "<th width='300' align='left'>Nome</th>";
-            echo "<th width='100' align='left'>CPF</th>";
-            echo "<th width='250' align='left'>E-mail</th>";
-            echo "<th width='50' align='left'>Editar</th>";
-        echo "</tr>"
-        while($linha=mysqli_fetch_array($resultado)) {
-            $codigo = $linha["codigo"];
-            $nome = $linha["nome"];
-            $cpf = $linha["cpf"];
-            $email = $linha["email"];
-            echo "<td width='100' align='right'>$codigo</td>";
-            echo "<td width='300' align='left'>$Nome</td>";
-            echo "<td width='100' align='left'>$cpf</td>";
-            echo "<td width='250' align='left'>$email</td>";
-            echo "<td width='50'<a href='clientes_pesquisar.php?codigo=$codigo'>Editar</td>";
-        }
-    echo "</table>"    
 </body>
 </html>
